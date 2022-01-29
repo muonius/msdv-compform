0c50633e4d3b17abac56ca2ab7560ba26d602f6a
Yang Zhao <42701448+muonius@users.noreply.github.com>
2022-01-28 20:48:50 -0500

    update

--- week-01/sketch/sketch.js
+++ week-01/sketch/sketch.js
@@ -3,7 +3,7 @@
 
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(1800, 1800);
     noStroke();
     fill(255, 255, 255);
     noLoop();
@@ -13,7 +13,7 @@ function draw() {
 
     const createGrid = () => {
         const points = [];
-        const count = 5;
+        const count = 50;
         for (let x = 0; x < count; x++) {
             for (let y = 0; y < count; y++) {
                 const u = count <= 1 ? 0.5 : x / (count - 1);
@@ -25,7 +25,19 @@ function draw() {
     }
 
     const points = createGrid();
+    let radius;
+    radius = random(0, 5);
+    const margin = 50;
 
-    console.log(points)
+    push();
+    points.forEach(([u, v]) => {
+        const x = lerp(margin, width - margin, u);
+        const y = lerp(margin, height - margin, v);
+        // console.log([x, y])
+        stroke(224, 0, 0);
+        fill(224, 0, 0);
+        ellipse(x, y, radius, radius);
+    })
+    pop();
 
 }
