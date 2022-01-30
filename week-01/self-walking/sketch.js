2740d9d912ea359d0cc27766d9185f8abe89d792
Yang Zhao <42701448+muonius@users.noreply.github.com>
2022-01-30 16:48:28 -0500

    sketch

--- week-01/self-walking/sketch.js
+++ week-01/self-walking/sketch.js
@@ -1,6 +1,9 @@
+// require https://cdn.jsdelivr.net/npm/p5@latest/lib/p5.min.js
 
 const points = [];
 const pos = [];
+let newPos;
+let offset = 120;
 
 function setup() {
     createCanvas(500, 500);
@@ -10,20 +13,43 @@ function setup() {
 }
 
 function draw() {
-    //1. Create grid of grid
-    for (let j = 0; j < 5; j++) {
-        push();
-        translate(0, j * 120)
-        for (let i = 0; i < 4; i++) {
-            createGrid(5, 3, 50)
-            translate(120, 0)
-            console.log(i, j)
-        }
-        pop();
+    function drawVertex(count, radius, margin, offset) {
 
+
+        //Create grid of grid
+        for (let j = 0; j < count; j++) {
+            push();
+            translate(0, j * offset)
+            for (let i = 0; i < 4; i++) {
+                createGrid(count, radius, margin)
+                translate(offset, 0)
+                // console.log(i, j)
+            }
+            pop();
+
+        }
+
+        //Use vertex to draw random connecting lines
+        for (let k = 0; k < count; k++) {
+            translate(k * offset, 0)
+
+            push();
+            for (let j = 0; j < (count - 1) * (count - 1); j++) {
+                translate(0, j * offset)
+                strokeWeight(1);
+                stroke(139, 0, 0);
+                beginShape(LINES);
+                for (let i = 0; i < count * count; i++) {
+                    newPos = shuffle(pos)
+                    vertex(newPos[i][0], newPos[i][1])
+                }
+                endShape();
+            }
+            pop();
+        }
     }
 
-
+    //Base grid function
     function createGrid(count, radius, margin) {
         for (let x = 0; x < count; x++) {
             for (let y = 0; y < count; y++) {
@@ -44,6 +70,8 @@ function draw() {
             pop();
         })
     }
+
+    drawVertex(5, 3, 50, 120)
+    // console.log(shuffle(pos.slice(0, 25)));
 }
 
-console.log(pos)
