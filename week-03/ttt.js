c944a11b0746d09e6daebc0eaef89b65f938127b
Yang Zhao <42701448+muonius@users.noreply.github.com>
2022-02-06 17:51:16 -0500

    update

--- week-03/ttt.js
+++ week-03/ttt.js
@@ -6,22 +6,38 @@ let board =
 
 let player1 = 'X';
 let player2 = 'O';
-let margin = 100;
+let band;
+let scale;
+let offset = 50;
+let innerWidth;
 
 function setup() {
     createCanvas(500, 500);
+
 }
 
 function draw() {
     background(220);
+    band = height / 3;
+    mark = height / 6;
+    innerWidth = width - mark;
 
+    for (let i = 1; i <= 2; i++) {
+        line(0, band * i, width, band * i)
+        line(band * i, 0, band * i, height)
+
+    }
     for (let i = 0; i < 3; i++) {
+
         for (let j = 0; j < 3; j++) {
+
             let spot = board[i][j];
-            let x = map(j, 0, 2, margin, width - margin)
-            let y = map(i, 0, 2, margin, height - margin)
+            let x = map(j, 0, 2, mark, innerWidth)
+            let y = map(i, 0, 2, mark, innerWidth)
             text(spot, x, y);
         }
+
+
     }
 
 }
