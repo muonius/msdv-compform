0b4f850e56d6fb885af210da81f47d094753db75
Yang Zhao <42701448+muonius@users.noreply.github.com>
2022-02-19 00:03:10 -0500

    update

---
+++ week-04/curve-params.js
@@ -0,0 +1,99 @@
+let scaleSlider, a1Slider, a2Slider, a3Slider, opSliderX, opSliderY;
+
+let operations = ["*", "+", "-"]
+
+function setup() {
+    createCanvas(500, 500)
+    angleMode(DEGREES)
+
+    createP("OperationsX")
+    opSliderX = createSlider(0, 3, 0)
+
+    createP("OperationsY")
+    opSliderY = createSlider(0, 3, 0)
+
+    // createP("scale")
+    // scaleSlider = createSlider(10, 40, 10)
+
+    createP("Multiplier a1")
+    a1Slider = createSlider(1, 10, 0.1)
+
+    createP("Multiplier a2")
+    a2Slider = createSlider(1, 10, 0.1)
+
+    createP("Multiplier a3")
+    a3Slider = createSlider(1, 10, 0.1)
+
+
+
+
+}
+
+function draw() {
+    background(255);
+    push();
+
+    translate(width / 2, height / 2)
+
+    stroke(0);
+    strokeWeight(1)
+
+    fill('red');
+    beginShape();
+    for (let a = 0; a < 360; a += radians(1)) {
+        // let r = scaleSlider.value()
+        let r = 10
+        let x1;
+        let y1;
+
+        if (opSliderX.value() == 3) {
+            x1 = r * 10 * sin(a + r) * sin(a * a2Slider.value() + r * a2Slider.value()) * (a * sin(a * a2Slider.value() * r * a3Slider.value()))
+        } else if (opSliderX.value() == 1) {
+            x1 = r * 10 * sin(a + r) * sin(a * a2Slider.value() + r * a2Slider.value()) + (a + sin(a * a2Slider.value() * r * a3Slider.value()))
+        }
+        else if (opSliderX.value() == 2) {
+            x1 = r * 10 * sin(a + r) * sin(a * a2Slider.value() + r * a2Slider.value()) - (a * sin(a * a2Slider.value() * r * a3Slider.value()))
+        }
+
+        else if (opSliderX.value() == 0) {
+            x1 = r * 16 * sin(a * a1Slider.value() + r * a1Slider.value()) * sin(a * a2Slider.value() + r * a2Slider.value()) * sin(a * a3Slider.value() + r * a3Slider.value())
+        }
+
+
+        if (opSliderY.value() == 3) {
+            y1 = -r * (13 * cos(a * a1Slider.value() + r * a1Slider.value()) + 15 * cos(2 * a2Slider.value() - r * a2Slider.value()) - 7 * cos(3 * a3Slider.value() + r * a3Slider.value()) * cos(4 * a2Slider.value()));
+        } else if (opSliderY.value() == 1) {
+            y1 = r * (13 * cos(a * a1Slider.value() + r * a1Slider.value()) * cos(3 * a2Slider.value() * r * a3Slider.value()) * cos(4 * a2Slider.value()));
+
+        }
+        else if (opSliderY.value() == 2) {
+            y1 = -r * (13 * cos(a * a1Slider.value() + r * a1Slider.value()) + cos(7 * a2Slider.value() * r * a3Slider.value()) * cos(4 * a2Slider.value()));
+        }
+
+        else if (opSliderY.value() == 0) {
+            y1 = -r * (13 * cos(a * a1Slider.value() + r * a1Slider.value()) + 9 * cos(2 * a2Slider.value() + r * a2Slider.value()) - 12 * cos(3 * a3Slider.value() + r * a3Slider.value()) * cos(4 * a2Slider.value()));
+        }
+
+
+        // let x1 = r * 16 * sin(a * a1Slider.value() + r * a1Slider.value()) * sin(a * a2Slider.value() + r * a2Slider.value()) * sin(a * a3Slider.value() + r * a3Slider.value())
+        // let y1 = -r * (13 * cos(a * a1Slider.value() + r * a1Slider.value()) + 9 * cos(2 * a2Slider.value() + r * a2Slider.value()) - 12 * cos(3 * a3Slider.value() + r * a3Slider.value()) * cos(4 * a2Slider.value()));
+
+
+        //standard heart shape
+        // let x1 = r * 16 * pow(sin(a), 3);
+        // let y1 = -r * (13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a));
+
+        vertex(x1, y1)
+        // vertex(x1, y1)
+
+    }
+
+    endShape(CLOSE);
+    pop();
+    // console.log("scaleSlider", scaleSlider.value())
+    // console.log("a1Slider", a1Slider.value())
+    // console.log("a2Slider", a2Slider.value())
+    // console.log("a3Slider", a3Slider.value())
+    //scaleSlider 10,7,7,4
+
+}
