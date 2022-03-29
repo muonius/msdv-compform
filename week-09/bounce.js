802845a8adc4856f066e0345405b551c7b49e108
Yang Zhao <freda.y.zhao@gmail.com>
2022-03-28 21:05:04 -0400

    update

--- week-09/bounce.js
+++ week-09/bounce.js
@@ -1,7 +1,7 @@
 // require https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js
 
+let x = 0;
 /*exported setup draw */
-
 function setup() {
   createCanvas(400, 400);
   frameRate(30);
@@ -21,9 +21,19 @@ function draw() {
   // const a = map(frameCount, 0, 60, 0, PI);
   //faster
   // const a = map(frameCount, 0, 5, 0, PI);
-  const a = map(frameCount, 0, 20, 0, PI);
+  //default
+  // const a = map(frameCount, 0, 20, 0, PI);
+  //once per second
+  const a = map(millis(), 0, 1000, 0, PI);
+  x = map(millis(), 0, 2000, 0, width);
   let offset = sin(a) * 50;
   offset = abs(offset);
   const y = 200 - offset;
-  ellipse(200, y, 100, 100);
+  //default
+  // ellipse(200, y, 100, 100);
+  ellipse(x, y, 100, 100);
+
+  if (x >= width) {
+    x = 0;
+  }
 }
