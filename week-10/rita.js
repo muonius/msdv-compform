05f05e32e47af285355cba40df3d6d3501258ad4
Yang Zhao <freda.y.zhao@gmail.com>
2022-04-09 13:51:52 -0400

    rita

--- week-10/rita.js
+++ week-10/rita.js
@@ -1,53 +1,106 @@
 const grammar = {
-  "<start>": ["<5-line> <br/> <7-line> <br/> <5-line>"],
-  "<5-line>": [
-    "<1> <4>",
-    "<1> <3> <1>",
-    "<1> <1> <3>",
-    "<1> <2> <2>",
-    "<1> <2> <1> <1>",
-    "<1> <1> <2> <1>",
-    "<2> <3>",
+  start: [
+    "$5line <br> $7line <br> $5line <br> <br> <br> $5line <br> $7line <br> $5line <br> <br> <br> $5line <br> $7line <br> $5line",
   ],
-  "<7-line>": [
-    "<1> <1> <5-line>",
-    "<2> <5-line>",
-    "<5-line> <2>",
-    "<5-line> <1> <1>",
+  "5line": [
+    "$one $four",
+    "$one $three $one",
+    "$one $one $three",
+    "$two $three",
+    "$two $three",
   ],
-  "<1>": [
+  "7line": [
+    "$one $one $5line",
+    "$two $5line",
+    "$5line $two",
+    "$5line $one $one",
+    "$four $two $one",
+    "$one $5line $one",
+    "$two $three $two",
+    "$four $three",
+    "$three $four",
+  ],
+  one: [
     "a",
     "the",
+    "ponders",
+    "his",
+    "her",
+    "their",
     "great",
     "profound",
-    "meta",
+    "consciousness",
     "time",
     "space",
     "wonder",
-    "is",
-    "are",
     "gets",
     "makes",
+    "does",
+    "did",
+    "hears",
+    "listens",
+    "sees",
+    "beauty",
+    "truth",
+    "goodness",
+    "happiness",
+    "love",
+    "says",
   ],
-  "<2>": ["rainy day", "virtual reality", "delicious meal"],
-  "<3>": ["no one knows", "how life works", "we keep pondering"],
-  "<4>": [
+  two: [
+    "a man",
+    "the man",
+    "a woman",
+    "the woman",
+    "the girl",
+    "the boy",
+    "reason why",
+    "never ends",
+    "everyday is",
+    "the world",
+    "nature is",
+    "the earth",
+    "who listens",
+    "who he",
+    "who she",
+  ],
+  three: [
+    "no one knows",
+    "how life works",
+    "we keep pondering",
+    "just getting started",
+    "never too late",
+    "bit by bit",
+    "the unsung heros",
+    "the flying fish",
+    "the little prince",
+    "connecting the dots",
+    "seizes every opportunity",
+  ],
+  four: [
     "the end starts now",
     "a circle closes itself",
     "the sun rises tomorrow",
+    "like peeling an onion",
+    "opportunity knocks but once",
   ],
 };
 
 const context = {};
 
 const g = RiTa.grammar(grammar, context);
+
+const title = document.createElement("div");
+title.innerHTML = "<h1> A Poem </h1>";
+document.body.append(title);
+
 show(g.expand());
 
 function show(...strings) {
   for (let s of strings) {
-    console.log(s);
+    // console.log(s);
     const div = document.createElement("div");
-    div.innerText = s;
+    div.innerHTML = s;
     document.body.append(div);
   }
 }
