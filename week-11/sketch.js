0ec183d7085f321232770366b043dfa8a901acd6
Yang Zhao <freda.y.zhao@gmail.com>
2022-04-16 00:23:32 -0400

    update

--- week-11/sketch.js
+++ week-11/sketch.js
@@ -1,12 +1,14 @@
 let mySound;
 let fft;
+let speed = 0;
 
 function preload() {
   mySound = loadSound("rapgod.mp3");
 }
 
 function setup() {
-  createCanvas(500, 200);
+  createCanvas(300, 300);
+  // colorMode(HSB);
 
   fft = new p5.FFT(0, 128);
   fft.setInput(mySound);
@@ -27,17 +29,20 @@ function stop() {
 }
 
 function draw() {
-  background(50);
-  stroke(255);
-  noFill();
-  strokeWeight(random(10));
+  background(0);
+  stroke(random(200), random(200), random(200));
+  translate(random(width / 2), random(height));
+  strokeWeight(random(20));
   const data = fft.waveform();
   beginShape();
-  for (let i = 0; i < TWO_PI; i += radians(1)) {
-    let r = map(noise(data[i]), 0, 1, 10, 50);
-    const x = r * cos(i);
-    const y = r * sin(i);
+  for (let i = 0; i < 2; i += 1) {
+    let r = map(noise(i * abs(data[i])), 0, 1, 0, 200);
+    let color = map(noise(i * abs(data[i])), 0, 1, 0, 255);
+    const x = r * cos(noise(i));
+    const y = r * sin(noise(i));
+    // console.log(color);
+    // fill(random(255), random(255), random(255));
     vertex(x, y);
   }
-  endShape();
+  endShape(CLOSE);
 }
