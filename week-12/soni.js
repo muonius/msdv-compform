e84bdfa0baf7cdfe35fcd014a7a1704195a588d5
Yang Zhao <freda.y.zhao@gmail.com>
2022-04-23 11:26:40 -0400

    update

--- week-12/soni.js
+++ week-12/soni.js
@@ -1,6 +1,7 @@
 //https://p5js.org/reference/#/p5/text
 let data;
-let osc;
+let osc1;
+let osc2;
 let year;
 let freq;
 let headingEl;
@@ -13,11 +14,15 @@ function preload() {
 }
 function setup() {
   createCanvas(500, 500);
-  temp = data.getColumn("January");
+  temp1 = data.getColumn("January");
+  temp2 = data.getColumn("Febrary");
   year = data.getColumn("Year");
-  osc = new p5.Oscillator();
-  osc.setType("sine");
-  osc.amp(0);
+  osc1 = new p5.Oscillator();
+  osc2 = new p5.Oscillator();
+  osc1.setType("sine");
+  osc2.setType("square");
+  osc1.amp(0);
+  osc2.amp(0);
   button = createButton("play/pause");
   button.mousePressed(toggle);
 }
@@ -39,24 +44,26 @@ function start() {
 
 function toggle() {
   if (!playing) {
-    osc.start();
-    osc.amp(0.5, 1);
-    playMusic(temp);
+    osc1.start();
+    osc1.amp(0.5, 1);
+    osc2.start();
+    osc2.amp(0.5, 1);
+    playMusic(temp1, temp2, year);
     playing = true;
   } else {
-    osc.stop();
-    osc.amp(0, 1);
+    osc1.stop();
+    osc1.amp(0, 1);
+    osc2.stop();
+    osc2.amp(0, 1);
     playing = false;
   }
 }
 
-function playMusic(music) {
-  for (let i = 0; i < music.length; i++) {
-    let value = map(music[i], 0, 100, 200, 600);
-    osc.freq(value);
-
-    let freq = osc.getAmp();
-    let m = freq / 100;
-    console.log(value);
+function playMusic(temp1, temp2, data) {
+  for (let i = 0; i < data.length; i++) {
+    let value1 = map(temp1[i], 1, 100, 10, 1000);
+    let value2 = map(temp2[i], 1, 200, 400, 1600);
+    osc1.freq(value1);
+    osc2.freq(value2);
   }
 }
